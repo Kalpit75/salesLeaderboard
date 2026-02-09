60efa0270a2440cffbb0605b07457f57075a0079
Kalpit75 <kalpitdhakal001@gmail.com>
2026-02-09 20:07:42 +0545

    deploy ready

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", require("./routes/sales"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port ${PORT}");
 });
 
